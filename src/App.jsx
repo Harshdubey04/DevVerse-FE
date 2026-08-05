ef0f3cf9b655f177c5e119ab9362553c3269cbf3
Harsh Dubey <harshdu001@gmail.com>
2026-08-05 10:31:51 +0530

    React Project Setup along with the tailwind css and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+    <button class="btn">Button</button>
+    <div className="text-red-700">Frontend Initialized</div>
+    </>
+  )
+}
+
+export default App
