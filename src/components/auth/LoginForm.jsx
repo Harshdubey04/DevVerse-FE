59b338537861d08dc1e9f03a615eee81ad2b134a
Harsh Dubey <harshdu001@gmail.com>
2026-08-05 16:38:50 +0530

    Integrated React Hook Form, Integrated Zod using zodResolver, Created loginSchema.js, Added client-side validation and Displayed validation error messages

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,51 +1,95 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "../../schemas/loginSchema";
+
+
+
 function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      emailId: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+
+
   return (
-    <div className="card w-full max-w-md bg-base-100 shadow-2xl">
-      <div className="card-body p-8">
+    <form onSubmit={handleSubmit(onSubmit)}>
 
-        <h2 className="text-3xl font-bold text-center">
-          Welcome Back 👋
-        </h2>
+      <div className="card w-full max-w-md bg-base-100 shadow-2xl">
+        <div className="card-body p-8">
 
-        <p className="text-center opacity-70 mb-6">
-          Sign in to continue to DevVerse
-        </p>
+          <h2 className="text-3xl font-bold text-center">
+            Welcome Back 👋
+          </h2>
 
-        <fieldset className="fieldset">
+          <p className="text-center opacity-70 mb-6">
+            Sign in to continue to DevVerse
+          </p>
 
-          <legend className="fieldset-legend">Email</legend>
+          <fieldset className="fieldset">
 
-          <input
-            type="email"
-            placeholder="Enter your email"
-            className="input input-bordered w-full"
-          />
+            <legend className="fieldset-legend">Email</legend>
 
-          <legend className="fieldset-legend mt-4">
-            Password
-          </legend>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className={`input input-bordered w-full ${errors.emailId ? "input-error" : ""
+                }`}
+              {...register("emailId")}
+            />
 
-          <input
-            type="password"
-            placeholder="Enter your password"
-            className="input input-bordered w-full"
-          />
+            {errors.emailId && (
+              <p className="text-error text-sm mt-1">
+                {errors.emailId.message}
+              </p>
+            )}
 
-        </fieldset>
+            <legend className="fieldset-legend mt-4">
+              Password
+            </legend>
 
-        <button className="btn btn-primary w-full mt-6">
-          Sign In
-        </button>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className={`input input-bordered w-full ${errors.password ? "input-error" : ""
+                }`}
+              {...register("password")}
+            />
 
-        <p className="text-center mt-6">
-          Don't have an account?{" "}
-          <button className="link link-primary font-semibold">
-            Sign Up
+            {errors.password && (
+              <p className="text-error text-sm mt-1">
+                {errors.password.message}
+              </p>
+            )}
+
+          </fieldset>
+
+          <button type="submit" className="btn btn-primary w-full mt-6">
+            Sign In
           </button>
-        </p>
 
+          <p className="text-center mt-6">
+            Don't have an account?{" "}
+            <button className="link link-primary font-semibold">
+              Sign Up
+            </button>
+          </p>
+
+        </div>
       </div>
-    </div>
+
+    </form>
   );
 }
 
